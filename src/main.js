601b77e146041e338fe53fb4e310eb5f98ce39ce
igor-ex <igorfado@gmail.com>
2019-03-31 00:17:04 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//Элемент-объект для хранения и отображения
+function Element(id, fName, lName, age) {
+    this.id = id;
+    this.lName = lName;
+    this.fName = fName;
+    this.age = age;
+}
+
+
