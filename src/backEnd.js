7b225fcda9df3fb165c20ee2774ca1cd7e8f7fca
igor-ex <igorfado@gmail.com>
2019-03-31 15:05:06 +0300

    добавил нереализованный объект BackEnd

---
+++ src/backEnd.js
@@ -0,0 +1,14 @@
+
+function BackEnd() {
+
+}
+
+//функции доступа к серверу принимают объект с данными и два коллбэка - при успеехе или неуспехе
+//операции. В колбэк будет положен объект данных
+BackEnd.prototype.logIn = function (data, successCallback, failCallback) {
+
+};
+
+BackEnd.prototype.signUp = function (data, successCallback, failCallback) {
+
+};
